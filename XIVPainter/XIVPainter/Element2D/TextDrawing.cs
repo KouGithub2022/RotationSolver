364daabc0363f3a94f86775fd874979835259d6c
KouGithub2022 <106080653+KouGithub2022@users.noreply.github.com>
2024-04-01 22:08:16 +0900

    Add files via upload

---
+++ XIVPainter/XIVPainter/Element2D/TextDrawing.cs
@@ -0,0 +1,25 @@
+﻿namespace XIVPainter.Element2D;
+
+/// <summary>
+/// Text drawing.
+/// </summary>
+/// <remarks>
+/// 
+/// </remarks>
+/// <param name="pt"></param>
+/// <param name="color"></param>
+/// <param name="text"></param>
+public readonly struct TextDrawing(Vector2 pt, uint color, string text) : IDrawing2D
+{
+    readonly Vector2 _pt = pt;
+    readonly uint _color = color;
+    readonly string _text = text;
+
+    /// <summary>
+    /// Draw on the <seealso cref="ImGui"/>
+    /// </summary>
+    public void Draw()
+    {
+        ImGui.GetWindowDrawList().AddText(_pt, _color, _text);
+    }
+}
